9521ab7351450398787fb3b53870f06e2b7b32bc
Eduardo Bravo <eduardojosebb.matescience@gmail.com>
2022-07-11 00:03:22 -0400

    add last changes in the code

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        int resultado;
-        System.out.println("Hello world!");
-        resultado = suma(10, 20, 30);
+        // 1era. salida del Programa
+        var resultado = suma(10, 20, 30);
         System.out.println(resultado);
+
+        // 2da. salida del Programa
+        Coche miCoche = new Coche();
+        miCoche.AgregarPuertas();
+        System.out.println(miCoche.puertas);
     }
 
     public static int suma(int a, int b, int c) {
         return a + b + c;
+    }
+}
+
+class Coche {
+    public int puertas = 4;
+
+    public void AgregarPuertas() {
+        this.puertas++;
     }
 }
