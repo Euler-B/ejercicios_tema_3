714ca0f4144c44b36709e98ee075a958c82d438e
Eduardo Bravo <eduardojosebb.matescience@gmail.com>
2022-07-10 23:18:47 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int resultado;
+        System.out.println("Hello world!");
+        resultado = suma(10, 20, 30);
+        System.out.println(resultado);
+    }
+
+    public static int suma(int a, int b, int c) {
+        return a + b + c;
+    }
+}
